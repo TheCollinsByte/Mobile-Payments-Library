2cdf5986921657874e27e9e9f5bef0cd6a87e92c
colspan <collin@collindigm.com>
2023-09-26 11:48:45 +0300

    Config clean-up

--- mpesa-mobile-payment-impl/src/main/java/com/kwawingu/payments/MpesaConfig.java
+++ mpesa-mobile-payment-impl/src/main/java/com/kwawingu/payments/MpesaConfig.java
@@ -29,15 +29,12 @@ import java.util.Optional;
 
 public class MpesaConfig implements MobilePayment {
     private static final Logger LOG = LoggerFactory.getLogger(MpesaConfig.class);
-
     private final String publicKey;
     private final String apiKey;
-    private final HttpClient httpClient;
 
     public MpesaConfig(String publicKey, String apiKey) {
         this.publicKey = publicKey;
         this.apiKey = apiKey;
-        httpClient = HttpClient.newHttpClient();
     }
 
     @Override
@@ -59,67 +56,6 @@ public class MpesaConfig implements MobilePayment {
 
     @Override
     public Optional<String> getSessionKey(String encryptedApiKey, String context) throws IOException {
-        HttpResponse<String> response;
-
-        HttpRequest request = buildSessionRequest(encryptedApiKey, context);
-        response = sendSessionRequest(request);
-
-        try {
-            handleSessionResponse(response);
-            return extractSessionKey(response.body());
-        } catch (IOException e) {
-            LOG.debug("Error Processing session response: {}", e.getMessage());
-        }
-
-        return Optional.empty();
-    }
-
-    private HttpRequest buildSessionRequest(String encryptedApiKey, String context) {
-        Map<String, String> headers = new HashMap<>();
-        headers.put("Content-Type", "application/json");
-        headers.put("Authorization", "Bearer " + encryptedApiKey);
-        headers.put("Origin", "*");
-
-        HttpRequest.Builder requestBuilder = HttpRequest.newBuilder()
-                .uri(URI.create(context))
-                .GET();
-
-        headers.forEach(requestBuilder::headers);
-
-        return requestBuilder.build();
-    }
-
-    private HttpResponse<String> sendSessionRequest(HttpRequest request) {
-        try {
-            return httpClient.send(request, HttpResponse.BodyHandlers.ofString());
-        } catch (IOException | InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    private void handleSessionResponse(HttpResponse<String> response) throws IOException {
-        int statusCode = response.statusCode();
-
-        if (statusCode != 200 && statusCode != 400) {
-            throw new IOException("Unexpected HTTP Code" + statusCode);
-        }
-
-        if (statusCode == 400) {
-            throw new IOException("Session Creation Failed: " + statusCode);
-        }
-    }
-
-    private Optional<String> extractSessionKey(String responseBody) {
-        if (responseBody != null) {
-            JsonElement jsonElement = JsonParser.parseString(responseBody);
-
-            if (jsonElement.isJsonObject()) {
-                JsonObject jsonObject = jsonElement.getAsJsonObject();
-                if (jsonObject.has("output_SessionID")) {
-                    return Optional.of(jsonObject.get("output_SessionID").getAsString());
-                }
-            }
-        }
         return Optional.empty();
     }
 }
