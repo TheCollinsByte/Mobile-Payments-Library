b987cb36d24ab1d57fbbcbbd5c9231c24004e57c
colspan <collin@collindigm.com>
2023-09-26 12:47:36 +0300

    API Endpint Type defined at the api level

---
+++ mobile-payment-api/src/main/java/com/kwawingu/payments/ApiEndpoint.java
@@ -0,0 +1,19 @@
+package com.kwawingu.payments;
+
+public class ApiEndpoint {
+    private final Environment environment;
+    private final Market market;
+
+    public ApiEndpoint(Environment environment, Market market) {
+        this.environment = environment;
+        this.market = market;
+    }
+
+    public String getUrl() {
+        return (environment.isSsl() ? "https://" : "http://") +
+                environment.getHost() + "/" +
+                environment.getName().toLowerCase() +
+                "/ipg/v2/" +
+                market.getContextValue();
+    }
+}
