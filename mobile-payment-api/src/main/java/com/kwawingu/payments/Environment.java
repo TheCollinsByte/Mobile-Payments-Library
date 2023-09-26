89276b3362ba84dcc34e54c7166e3b0d11b13553
colspan <collin@collindigm.com>
2023-09-26 12:31:53 +0300

    Environment Type: btwn PROD and DEV (Sandbox and OpenAPI)

---
+++ mobile-payment-api/src/main/java/com/kwawingu/payments/Environment.java
@@ -0,0 +1,34 @@
+package com.kwawingu.payments;
+
+public enum Environment {
+    SANDBOX("Sandbox", true, "openapi.m-pesa.com", 443),
+    OPENAPI("OpenAPI", true, "openapi.m-pesa.com", 443);
+
+    private final String name;
+    private final boolean ssl;
+    private final String host;
+    private final int port;
+
+    Environment(String name, boolean ssl, String host, int port) {
+        this.name = name;
+        this.ssl = ssl;
+        this.host = host;
+        this.port = port;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public boolean isSsl() {
+        return ssl;
+    }
+
+    public String getHost() {
+        return host;
+    }
+
+    public int getPort() {
+        return port;
+    }
+}
