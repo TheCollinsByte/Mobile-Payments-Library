df2dd956b0cbcf9020fd2ca0cb06bdbfe99dd4ab
colspan <collin@collindigm.com>
2023-10-06 22:53:13 +0300

    Service API: prop added to the enum

--- mobile-payment-api/src/main/java/com/kwawingu/payments/Service.java
+++ mobile-payment-api/src/main/java/com/kwawingu/payments/Service.java
@@ -4,15 +4,16 @@
 package com.kwawingu.payments;
 
 public enum Service {
-  GET_SESSION("/getSession/");
+  GET_SESSION("/getSession/"),
+  CUSTOMER_TO_BUSINESS("/c2bPayment/singleStage/");
 
-  private final String path;
+  private final String service;
 
-  Service(String path) {
-    this.path = path;
+  Service(String service) {
+    this.service = service;
   }
 
   public String getPath() {
-    return path;
+    return service;
   }
 }
