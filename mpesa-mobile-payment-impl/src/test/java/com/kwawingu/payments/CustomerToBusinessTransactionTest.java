2b5831c9db1316208cdd2fa2dbd4c92746b6848c
colspan <collin@collindigm.com>
2023-11-07 23:32:46 +0300

    Clean-up: Moved the logic to the main library code

--- mpesa-mobile-payment-impl/src/test/java/com/kwawingu/payments/CustomerToBusinessTransactionTest.java
+++ mpesa-mobile-payment-impl/src/test/java/com/kwawingu/payments/CustomerToBusinessTransactionTest.java
@@ -2,14 +2,15 @@ package com.kwawingu.payments;
 
 import com.kwawingu.payments.C2B.CustomerToBusinessTransaction;
 import com.kwawingu.payments.C2B.Payload;
-import org.junit.jupiter.api.BeforeEach;
+import com.kwawingu.payments.Session.MpesaKeyProviderFromEnvironment;
+import com.kwawingu.payments.Session.MpesaSession;
+import com.kwawingu.payments.exception.SessionKeyUnavailableException;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.util.Arrays;
-import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -17,49 +18,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class CustomerToBusinessTransactionTest {
     private static final Logger LOG = LoggerFactory.getLogger(CustomerToBusinessTransactionTest.class);
-    private CustomerToBusinessTransaction customerToBusinessTransaction;
-
-    Payload payload = new Payload.Builder()
-            .setAmount("10.00")
-            .setCustomerMSISDN("+255762578467")
-            .setCountry(Market.VODACOM_TANZANIA.getInputCountryValue())
-            .setCurrency(Market.VODACOM_TANZANIA.getInputCurrencyValue())
-            .setServiceProviderCode("000000")
-            .setTransactionReference("T1234C")
-            .setThirdPartyConversationID("asv02e5958774f783d0d689761")
-            .setPurchasedItemsDesc("Handbag, Black shoes")
-            .build();
-
-    @BeforeEach
-    public void setUp() {
-        String publicKey = System.getenv("MPESA_PUBLIC_KEY");
-        String apiKey = System.getenv("MPESA_API_KEY");
-
-        if (publicKey == null || apiKey == null) {
-            throw new RuntimeException(
-                    "Missing environment variables: MPESA_PUBLIC_KEY or MPESA_API_KEY");
-        }
-
-        ApiEndpoint apiEndpoint = new ApiEndpoint(Environment.SANDBOX, Market.VODACOM_TANZANIA);
-        GenerateSessionKey sessionKey = new GenerateSessionKey();
-        EncryptApiKey encryptApiKey = new EncryptApiKey(publicKey, apiKey);
-
-        String context = apiEndpoint.getUrl(Service.GET_SESSION);
-        String anEncryptedApiKey = encryptApiKey.generateAnEncryptApiKey();
-        LOG.info(context);
-
-        assertNotNull(anEncryptedApiKey);
-        LOG.info(payload.toJsonString());
-
-        Optional<String> generatedSessionKey = sessionKey.getSessionKey(anEncryptedApiKey, context);
-        assertTrue(generatedSessionKey.isPresent());
-        String encryptedSessionKey = encryptApiKey.generateAnEncryptSessionKey(generatedSessionKey.orElse(null));
-        customerToBusinessTransaction = new CustomerToBusinessTransaction.Builder()
-                .setApiEndpoint(apiEndpoint)
-                .setEncryptedSessionKey(encryptedSessionKey)
-                .setPayload(payload)
-                .build();
-    }
+    private final MpesaSession session = new MpesaSession(new MpesaKeyProviderFromEnvironment(), Environment.SANDBOX, Market.VODACOM_TANZANIA);
 
     private void printSanitizeResponse(String response) {
         for (String s : response.split(",")) {
@@ -71,10 +30,30 @@ public class CustomerToBusinessTransactionTest {
     }
 
     @Test
-    public void testPayment_wheInitiated_responseSucceed() throws IOException, InterruptedException {
-        String response = customerToBusinessTransaction.initiatePayment();
+    public void testPayment_wheInitiated_responseSucceed() throws IOException, InterruptedException, SessionKeyUnavailableException {
+        // Set-Up
+        Payload payload = new Payload.Builder()
+                .setAmount("10.00")
+                .setCustomerMSISDN("+255762578467")
+                .setCountry(Market.VODACOM_TANZANIA.getInputCountryValue())
+                .setCurrency(Market.VODACOM_TANZANIA.getInputCurrencyValue())
+                .setServiceProviderCode("000000")
+                .setTransactionReference("T1234C")
+                .setThirdPartyConversationID("asv02e5958774f783d0d689761")
+                .setPurchasedItemsDesc("Handbag, Black shoes")
+                .build();
+
+        CustomerToBusinessTransaction customerToBusinessTransaction = new CustomerToBusinessTransaction.Builder()
+                .setApiEndpoint(new ApiEndpoint(Environment.SANDBOX, Market.VODACOM_TANZANIA))
+                .setEncryptedSessionKey(session.getEncryptedSessionKey())
+                .setPayload(payload)
+                .build();
+
+        // Test
+        String response = customerToBusinessTransaction.synchronousPayment();
         printSanitizeResponse(response);
 
+        // Assertion
         assertNotNull(response);
         assertFalse(response.isBlank());
     }
