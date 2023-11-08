f4767faa036de2b016870e5c282e81e51ccb5887
colspan <collin@collindigm.com>
2023-11-08 15:05:08 +0300

    Providing an API to the end-user to provider there custom API Key and Public Key

--- mpesa-mobile-payment-impl/src/main/java/com/kwawingu/payments/session/MpesaKeyProviderFromEnvironment.java
+++ mpesa-mobile-payment-impl/src/main/java/com/kwawingu/payments/session/MpesaKeyProviderFromEnvironment.java
@@ -1,13 +1,20 @@
 package com.kwawingu.payments.session;
 
 public class MpesaKeyProviderFromEnvironment implements MpesaKeyProvider {
+
+    private final Config config;
+
+    public MpesaKeyProviderFromEnvironment(Config config) {
+        this.config = config;
+    }
+
     @Override
     public String getApiKey() {
-        return System.getenv("MPESA_API_KEY");
+        return System.getenv(config.getMpesaApiKey().getApiKey());
     }
 
     @Override
     public String getPublicKey() {
-        return System.getenv("MPESA_PUBLIC_KEY");
+        return System.getenv(config.getMpesaPublicKey().getPublicKey());
     }
 }
