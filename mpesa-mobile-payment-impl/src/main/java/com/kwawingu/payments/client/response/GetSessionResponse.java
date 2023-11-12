e165f0244357e9c51b8b4b8933f13077b79c4719
colspan <collin@collindigm.com>
2023-11-12 23:59:37 +0300

    Helper method

--- mpesa-mobile-payment-impl/src/main/java/com/kwawingu/payments/client/response/GetSessionResponse.java
+++ mpesa-mobile-payment-impl/src/main/java/com/kwawingu/payments/client/response/GetSessionResponse.java
@@ -1,13 +1,29 @@
 package com.kwawingu.payments.client.response;
 
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import com.kwawingu.payments.session.keys.MpesaSessionKey;
 
 import java.net.http.HttpResponse;
 
-public class GetSessionResponse {
-    private final MpesaSessionKey output_SessionID;
+public class GetSessionResponse extends MpesaHttpResponse {
+    private MpesaSessionKey output_SessionID;
 
-    protected GetSessionResponse(HttpResponse<String> response) {
-        output_SessionID = new MpesaSessionKey("");
+    public GetSessionResponse(HttpResponse<String> response) {
+        JsonObject jsonObject = util(response.body());
+        super(jsonObject.get("output_ResponseCode").getAsString(), jsonObject.get("output_ResponseDesc").getAsString());
+        output_SessionID = new MpesaSessionKey(jsonObject.get("output_SessionID").getAsString());
+    }
+
+    public JsonObject util(String response) {
+        try {
+            JsonElement jsonElement = JsonParser.parseString(response);
+            if (jsonElement.isJsonObject())
+                return jsonElement.getAsJsonObject();
+        } catch (Exception e) {
+            throw new IllegalStateException(e);
+        }
+        return new JsonObject();
     }
 }
