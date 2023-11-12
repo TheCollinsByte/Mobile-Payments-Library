6cce95a94fee6a7be5a69997c1da51975aa0052e
colspan <collin@collindigm.com>
2023-11-13 00:26:01 +0300

    Mpesa http client Type update with string to json parser response util method

--- mpesa-mobile-payment-impl/src/main/java/com/kwawingu/payments/client/MpesaHttpClient.java
+++ mpesa-mobile-payment-impl/src/main/java/com/kwawingu/payments/client/MpesaHttpClient.java
@@ -1,5 +1,9 @@
 package com.kwawingu.payments.client;
 
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.kwawingu.payments.client.response.GetSessionResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -19,14 +23,26 @@ public class MpesaHttpClient {
         this.httpClient = HttpClient.newHttpClient();
     }
 
-    public HttpResponse<String> getRequest(Map<String, String> headers, URI uri) throws IOException, InterruptedException {
+    public JsonObject stringToJson(String response) {
+        try {
+            JsonElement jsonElement = JsonParser.parseString(response);
+            if (jsonElement.isJsonObject())
+                return jsonElement.getAsJsonObject();
+        } catch (Exception e) {
+            throw new IllegalStateException(e);
+        }
+        return new JsonObject();
+    }
+
+    public GetSessionResponse getSessionRequest(Map<String, String> headers, URI uri) throws IOException, InterruptedException {
         HttpRequest.Builder requestBuilder = HttpRequest
                 .newBuilder()
                 .uri(uri)
                 .GET();
         headers.forEach(requestBuilder::headers);
         HttpRequest request = requestBuilder.build();
-        return httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+        HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+        return new GetSessionResponse(response.statusCode(), stringToJson(response.body()));
     }
 
     public HttpResponse<String> postRequest(Map<String, String> headers, HttpRequest.BodyPublisher httpBody, URI uri) throws IOException, InterruptedException {
