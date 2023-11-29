3c06310ce16b81aa1f40d61ce957bbdba3b580dd
colspan <collin@collindigm.com>
2023-11-29 17:52:04 +0300

    Script Update to run the Unit Test

--- scripts/ci/kw-mobile-lib-format-static-analysis-unit-test.sh
+++ scripts/ci/kw-mobile-lib-format-static-analysis-unit-test.sh
@@ -27,3 +27,19 @@ if [ $exitStatus -ne 0 ]; then
     echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
     exit $exitStatus
 fi
+
+./gradlew --profile --continue --parallel --max-workers=8 clean check "$@"
+exitStatus=$?
+
+# Make unit test reports available for Github Actions to pickup
+DEST="build/ci/backend-format-static-analysis-unit-test"
+rm -rf $DEST
+mkdir -p $DEST
+find . -type f -regex ".*/build/test-results/.*xml" -exec cp --target-directory=$DEST {} +
+
+if [ $exitStatus -ne 0 ]; then
+    echo -e "\n\n!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+    echo "One or more backend unit tests failed; fix to continue."
+    echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+    exit $exitStatus
+fi
